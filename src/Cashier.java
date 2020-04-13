667089cd9ada2989ca1fb444e1832a7974ab71a7
ryanhelgeson <ryanhelgeson@192.168.0.132>
2020-04-12 20:01:21 -0500

    Get and sets for Cashier

--- src/Cashier.java
+++ src/Cashier.java
@@ -1,1 +1,40 @@
+public class Cashier {
+	private int id;
+	private String name;
+	private String username;
+	private String password;
+	
+	public int getId() {
+		return this.id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public void setPassword(int password) {
+		this.password = password;
+	}
+	
+}
 
