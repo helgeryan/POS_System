397f2d1f3e4632c964d5f53243245cebdff644d6
jastar556 <58493157+jastar556@users.noreply.github.com>
2020-04-25 16:15:53 -0500

    Corrected name to be First name, last name, updated toString method.

--- src/User.java
+++ src/User.java
@@ -1,22 +1,18 @@
 public class User {
-    private int id;
-    private String name;
-    private String username;
-    private String password;
-    private String position;
+    int empID;
+    String firstName;
+    String lastName;
+    String position;
+    String username;
+    String password;
 
-    User(String username, String password){
-        this.username = username;
-        this.password = password;
-    }
-
-    User(int id, String name, String position, String username, String password) {
-        this.id = id;
-        this.name = name;
-        this.position = position;
-        this.username = username;
-        this.password = password;
-
+    User(int EmpID, String FirstName, String LastName, String Position, String Username, String Password) {
+        this.empID = EmpID;
+        this.firstName = FirstName;
+        this.lastName = LastName;
+        this.position = Position;
+        this.username = Username;
+        this.password = Password;
     }
 
     public String getUsername() {
@@ -27,8 +23,48 @@ public class User {
         return password;
     }
 
+    public int getEmpID() {
+        return empID;
+    }
+
+    public void setEmpID(int empID) {
+        this.empID = empID;
+    }
+
+    public String getfirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String name) {
+        this.firstName = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
     @Override
     public String toString() {
-        return "User [id=" + id + ", name=" + name + ", username=" + username + ", password=" + password + "]";
+        return empID + ","+ firstName + "," + lastName + "," +  position + "," +  username  + "," +  password;
     }
 }
