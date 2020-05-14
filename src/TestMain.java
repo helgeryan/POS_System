192d06d05f56e1f18de6d16f5ae951b3682ff2e9
jastar556 <58493157+jastar556@users.noreply.github.com>
2020-05-13 20:53:24 -0500

    Added TestMain back in for Ali to review.

---
+++ src/TestMain.java
@@ -0,0 +1,42 @@
+import java.io.FileNotFoundException;
+
+public class TestMain {
+    public static void main(String[] args) throws FileNotFoundException {
+        // TODO Auto-generated method stub
+        POS_System p = new POS_System();
+        p.addRegister(new Register());
+        p.getRegisters().get(0).setCurrUser(new User(1, "Ryan", "Helgeson", "cashier", "user1", "password1", true));
+        p.getRegisters().get(0).newSale();
+        p.getRegisters().get(0).addItemToSale(new Item("Book",1, 5.99,3,5,"barnesandnobles", 0 ), 2);
+        p.getRegisters().get(0).addItemToSale(new Item("Book",1, 5.99,3,5,"barnesandnobles", 0 ), 2);
+        p.getRegisters().get(0).addItemToSale(new Item("Book",1, 5.99,3,5,"barnesandnobles", 0 ), 2);
+        p.getRegisters().get(0).addItemToSale(new Item("Book",1, 5.99,3,5,"barnesandnobles", 0 ), 2);
+        p.getRegisters().get(0).closeSale();
+
+        p.getRegisters().get(0).newSale();
+        p.getRegisters().get(0).addItemToSale(new Item("Book",1, 5.99,3,5,"barnesandnobles", 0 ), 3);
+        p.getRegisters().get(0).addItemToSale(new Item("Book",1, 5.99,3,5,"barnesandnobles", 0 ), 3);
+        p.getRegisters().get(0).addItemToSale(new Item("Book",1, 5.99,3,5,"barnesandnobles", 0 ), 3);
+        p.getRegisters().get(0).addItemToSale(new Item("Book",1, 5.99,3,5,"barnesandnobles", 0 ), 3);
+        p.getRegisters().get(0).closeSale();
+        //p.getRegisters().get(0).printSales();
+        //Return entire sale
+        //p.getRegisters().get(0).returnEntireSale(1);
+
+        //return a set of items
+        //p.getRegisters().get(0).returnSetofItems(2, 1,1,1,1,1,1);
+
+        //p.getRegisters().get(0).printSales();
+        //System.out.println("Done");
+
+        //System.out.println(p.getRegisters().get(0).getSale(1));
+        //System.out.println(p.getRegisters().get(0).getSale(2));
+
+        Reports r = new Reports();
+        //r.printInventoryBelowThreshold();
+        //r.printCurrentInventory();
+        //System.out.println();
+
+        //r.printSales(p.getRegisters().get(0).getSales());
+    }
+}
